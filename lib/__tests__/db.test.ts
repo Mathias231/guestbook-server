ce7c453dbfe8c0de89c4250bda9dbad43e32d58e
Mathias231 <mathias.krogh@outlook.com>
2022-10-12 13:14:14 +0000

    Making tests for create, update, delete posts

--- lib/__tests__/db.test.ts
+++ lib/__tests__/db.test.ts
@@ -1,8 +1,16 @@
-import mongoose, { Types } from 'mongoose';
+import mongoose, { isObjectIdOrHexString, Types } from 'mongoose';
 import { IUser } from '../../models/user.model';
-import connect, { createUser, deleteUser, updateUser } from '../db';
+import connect, {
+  createPost,
+  createUser,
+  deletePost,
+  deleteUser,
+  updatePost,
+  updateUser,
+} from '../db';
 import { v4 as uuidv4 } from 'uuid';
 import { IPost } from '../../models/post.model';
+import { OperationCanceledException } from 'typescript';
 
 describe('User creation', () => {
   beforeAll((done) => {
@@ -31,6 +39,47 @@ describe('User creation', () => {
     expect(user).toHaveProperty('password');
     return;
   });
+  afterAll((done) => {
+    mongoose.connection.close();
+    done();
+  });
+});
+
+describe('Post creation', () => {
+  beforeAll((done) => {
+    done();
+  });
+  let postObj: IPost;
+  let user = createUser({ username: uuidv4(), password: 'Passord1' });
+  it('Should create a post with title and content', async () => {
+    let resolvedUser = await user;
+    let post = await createPost({
+      userId: resolvedUser._id.toString(),
+      title: 'Test Title',
+      content: 'Amazing Content',
+    });
+    postObj = post;
+    expect(post).toHaveProperty('title');
+    expect(post).toHaveProperty('content');
+    return;
+  });
+
+  it('Should update a post with title and content', async () => {
+    let post = await updatePost(postObj._id.toString(), {
+      title: 'Utrolig endring!',
+      content: 'Endring er gjort!',
+    });
+    expect(post).toHaveProperty('title');
+    expect(post).toHaveProperty('content');
+  });
+
+  it('Should delete a post by id', async () => {
+    let post = await deletePost(postObj._id.toString());
+    expect(post).toHaveProperty('title');
+    expect(post).toHaveProperty('content');
+    return;
+  });
+
   afterAll((done) => {
     mongoose.connection.close();
     done();
